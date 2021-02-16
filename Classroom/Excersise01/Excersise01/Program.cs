f639fa1720683d72dc333fe0226504ec95e6d558
Pavel Klecansky <klecanskypavel@gmail.com>
2021-02-16 19:56:02 +0100

    Exercises from classroom

---
+++ Classroom/Excersise01/Excersise01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Excersise01
+{
+
+    
+    
+    class Program
+    {
+
+
+        public class A
+        {
+            
+        }
+
+        public class B : A
+        {
+            
+        }
+        
+        public int Type { get; set; }     
+        
+        static void foo(int i)
+        {
+            Console.WriteLine(i);
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] ar = {1, 3, 5};
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                ar[i] += 3;
+            }
+
+            int? someInteger = 3;
+            foo(someInteger.Value);
+
+            var a = new A();
+            var b = new B();
+
+            A bRef = b;
+            
+
+
+        }
+    }
+}
